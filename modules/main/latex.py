2ef07009699291b69b0f257bcd81a52a0bb424cb
pheineke <heineke.pe@gmail.com>
2023-11-29 20:12:41 +0100

    ..s.s

--- modules/main/latex.py
+++ modules/main/latex.py
@@ -1,5 +1,6 @@
 from sympy import *
 import matplotlib.pyplot as plt
+from PIL import Image
 
 import os
 import re
@@ -11,8 +12,26 @@ class Latex(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    
+
     @commands.Cog.listener("on_message")
     async def latex_converter(self,message):
+        def imagebgtransparent(image):
+        img = Image.open(image)
+        img = img.convert("RGBA")
+        datas = img.getdata()
+
+        newData = []
+        for item in datas:
+            if item[0] == 255 and item[1] == 255 and item[2] == 255:
+                newData.append((255, 255, 255, 0))
+            else:
+                newData.append(item)
+
+        img.putdata(newData)
+        img.save(image, "PNG")
+
+
         string = message.content
         if bool(re.search(r"\\[a-zA-Z]+", string)):
             
@@ -27,9 +46,10 @@ class Latex(commands.Cog):
                     fontsize=16,
                 )
 
-                fig.savefig("latexequation.png")
+                fig.savefig("lateximg.png")
+                imagebgtransparent("lateximg.png")
 
-                file = discord.File("./latexequation.png")
+                file = discord.File("./lateximg.png")
                 await message.channel.send(file=file)
             except Exception as e:
                 print("Konnte nicht konvertieren.")
