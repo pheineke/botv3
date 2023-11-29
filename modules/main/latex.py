eae40f9aa36b3dc2db52ded3e6a1f726f2bd3827
pheineke <heineke.pe@gmail.com>
2023-11-29 21:45:32 +0100

    s

--- modules/main/latex.py
+++ modules/main/latex.py
@@ -16,47 +16,48 @@ class Latex(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def latex_converter(self,message):
-        def imagebgtransparent(image):
-            img = Image.open(image)
+        def latex_converter(string):
+            if bool(re.search(r"\\[a-zA-Z]+", string)):
+                
+                try:
+                    fig = plt.figure(figsize=(3, 0.5))  # Dimensions of figsize are in inches
+                    text = fig.text(
+                        x=0.5,  # x-coordinate to place the text
+                        y=0.5,  # y-coordinate to place the text
+                        s=string,
+                        horizontalalignment="center",
+                        verticalalignment="center",
+                        fontsize=24,
+                    )
+                    fig.savefig("lateximg.png")
+                    imagebgtransparent("lateximg.png")
+
+                except Exception as e:
+                    print("Konnte nicht konvertieren.")
+
+        def imagebgtransparent():
+            img = Image.open("lateximg.png")
             img = img.convert("RGBA")
             datas = img.getdata()
 
             newData = []
             for item in datas:
-                if item[0] == 255 and item[1] == 255 and item[2] == 255:
+                if item[0] >= 200 and item[1] >= 200 and item[2] >= 200:
                     newData.append((255, 255, 255, 0))
-                elif item[0] != item[1] or item[1] != item[2]:
-                    newData.append((0, 0, 0, item[3]))
                 else:
-                    newData.append(item)
+                    newData.append((255, 255, 255, 255))
 
             img.putdata(newData)
-            img.save(image, "PNG")
+            img.save("lateximg.png", "PNG")
 
 
-        string = message.content
-        if bool(re.search(r"\\[a-zA-Z]+", string)):
-            
-            try:
-                fig = plt.figure(figsize=(3, 0.5))  # Dimensions of figsize are in inches
-                text = fig.text(
-                    x=0.5,  # x-coordinate to place the text
-                    y=0.5,  # y-coordinate to place the text
-                    s=string,
-                    horizontalalignment="center",
-                    verticalalignment="center",
-                    fontsize=16,
-                )
+        latex_converter(message.content)
+        imagebgtransparent()
 
-                fig.savefig("lateximg.png")
-                imagebgtransparent("lateximg.png")
+        file = discord.File
+        await message.channel.send(file=file)
 
-                file = discord.File("./lateximg.png")
-                await message.channel.send(file=file)
-            except Exception as e:
-                print("Konnte nicht konvertieren.")
-                print(e)
-                print(string)
+        os.remove("./lateximg.png")
 
 async def setup(bot):
     await bot.add_cog(Latex(bot))
