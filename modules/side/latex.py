e1117a1d2237096ef01b9a7cd3c791fac789bff5
pheineke <heineke.pe@gmail.com>
2023-11-29 18:58:21 +0100

    latexconverter

---
+++ modules/side/latex.py
@@ -0,0 +1,26 @@
+from sympy import *
+
+import os
+import re
+import discord
+from discord.ext import commands
+
+
+class Latex(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener("on_message")
+    async def latex_converter(self,message):
+        string = "\\" + message.content.lower()
+        if bool(re.search(r"\\[a-zA-Z]+", string)):
+            
+            try:
+                preview(fr'$${string}$$', viewer='file', filename='latexequation.png', euler=False, dvioptions=['-D','300'])
+                file = discord.File("./latexequation.png")
+                await message.channel.send(file=file)
+            except:
+                print("Konnte nicht konvertieren.")
+
+async def setup(bot):
+    await bot.add_cog(Latex(bot))
