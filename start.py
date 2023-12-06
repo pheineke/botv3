91a22ff9897c8e6090e8c8113fc3feb280c4652b
pheineke <heineke.pe@gmail.com>
2023-12-06 17:24:54 +0100

    del start

--- start.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-if os.popen('python3 --version').read() == "Python 3.6.8":
-        os.system('export PATH=~/.localpython/bin:$PATH')
-if os.popen('python3 --version').read() == "Python 3.11.0":
-        os.system('python3 main.py')
