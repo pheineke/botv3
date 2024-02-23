5b694b797499bc1aa351e2be8f01ad4a55511f05
pheineke <heineke.pb@edu.rptu.de>
2024-02-23 14:59:43 +0100

    system

---
+++ modules/main/System.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class System(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def savechat(self, ctx):
+        async for message in ctx.channel.history(limit=None):
+            user_name = message.author.name
+            message_content = message.content
+
+            with open(f"{ctx.channel.name}_messages.txt", "a") as file:
+                file.write(f'{{ "{user_name}" : "{message_content}" }}\n')
+
+async def setup(bot):
+    await bot.add_cog(System(bot))
