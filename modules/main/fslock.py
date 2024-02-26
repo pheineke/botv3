5dd17874a1193e26cafaa8f2be2ea16aa30562c4
p_heineke <heineke.pe@gmail.com>
2024-02-26 20:30:06 +0100

    exception

--- modules/main/fslock.py
+++ modules/main/fslock.py
@@ -30,40 +30,43 @@ class Fslock(commands.Cog):
         #datum0 = "2024-02-26"
         #datum1 = "2024-02-26"
         # Laden der Daten aus der Datei
-        data = np.loadtxt('lock-log.txt', delimiter=',',dtype=str)
+        try:
+            data = np.loadtxt('lock-log.txt', delimiter=',',dtype=str)
 
-        values = [row[2] == 'True' for row in data]
-        dates = [datetime.strptime(row[0] + ' ' + row[1], '%Y-%m-%d %H:%M') for row in data]
-        date_values = [[dates[i], values[i]] for i in range(len(dates))]
+            values = [row[2] == 'True' for row in data]
+            dates = [datetime.strptime(row[0] + ' ' + row[1], '%Y-%m-%d %H:%M') for row in data]
+            date_values = [[dates[i], values[i]] for i in range(len(dates))]
 
-        if not(datum0 and datum1):
-            filtered_entries = date_values
-        elif datum0 and not datum1:
-            filtered_entries = [entry for entry in date_values if datum0 in str(entry[0])]
-        elif datum0 and datum1:
-            filtered_entries = [entry for entry in date_values if datum0 in str(entry[0]) or datum1 in str(entry[0])]
-        else:
-            filtered_entries = date_values
-
-
-        plot_dates = [str(x[0]) for x in filtered_entries]
-        plot_values = [str(x[1]) for x in filtered_entries]
-        # Erstellen des Diagramms
-        plt.figure(figsize=(20, 3))
-        plt.plot(plot_dates, plot_values, marker='o', linestyle='-', color='b')
-        plt.xlabel('Date and Time')
-        plt.ylabel('Value')
-        plt.title('FS-Info Öffnungsverlauf')
-        plt.yticks([0, 1], ['OPEN', 'CLOSED'])
-        plt.grid(True)
-        plt.xticks(rotation=45)
-        plt.gca().invert_yaxis()  # Umkehren der y-Achse
-        plt.tight_layout()
-        plt.savefig("plot-door.png")
+            if not(datum0 and datum1):
+                filtered_entries = date_values
+            elif datum0 and not datum1:
+                filtered_entries = [entry for entry in date_values if datum0 in str(entry[0])]
+            elif datum0 and datum1:
+                filtered_entries = [entry for entry in date_values if datum0 in str(entry[0]) or datum1 in str(entry[0])]
+            else:
+                filtered_entries = date_values
 
 
-        await ctx.send(file=discord.File('plot-door.png'))
+            plot_dates = [str(x[0]) for x in filtered_entries]
+            plot_values = [str(x[1]) for x in filtered_entries]
+            # Erstellen des Diagramms
+            plt.figure(figsize=(20, 3))
+            plt.plot(plot_dates, plot_values, marker='o', linestyle='-', color='b')
+            plt.xlabel('Date and Time')
+            plt.ylabel('Value')
+            plt.title('FS-Info Öffnungsverlauf')
+            plt.yticks([0, 1], ['OPEN', 'CLOSED'])
+            plt.grid(True)
+            plt.xticks(rotation=45)
+            plt.gca().invert_yaxis()  # Umkehren der y-Achse
+            plt.tight_layout()
+            plt.savefig("plot-door.png")
 
+
+            await ctx.send(file=discord.File('plot-door.png'))
+
+        except Exception as e:
+            await ctx.send(f"No Data\n```{e}```")
 
 
 
