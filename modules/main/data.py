3463ac834abc6faffa9299adc2b06ec02c0f0d21
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-02-28 20:56:09 +0100

    data

---
+++ modules/main/data.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Data(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def getuserinfo(self, ctx):
+        member = ctx.author
+        await ctx.send(f"Benutzername: {member.name}")
+        await ctx.send(f"Benutzer-ID: {member.id}")
+        await ctx.send(f"Status: {member.status}")
+        await ctx.send(f"Avatar-URL: {member.avatar_url}")
+        await ctx.send("Rollen:")
+        for role in member.roles:
+            await ctx.send(role.name)
+
+async def setup(bot):
+    await bot.add_cog(Data(bot))
