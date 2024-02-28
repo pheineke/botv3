a79fd0c1d96624abeea57c382ca19db59224a8ad
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-02-28 22:13:31 +0100

    -

--- modules/main/data.py
+++ modules/main/data.py
@@ -13,48 +13,50 @@ class Data(commands.Cog):
         if not os.path.isdir("./userdata"):
             os.makedirs("./userdata")
 
+        accent_color = member.accent_color
+        accent_colour = member.accent_colour
+        activities = member.activities
+        activity = member.activity
+        avatar = member.avatar
+        banner = member.banner
+        bot = member.bot
+        color = member.color
+        colour = member.colour
+        created_at = member.created_at
+        default_avatar = member.default_avatar
+        desktop_status = member.desktop_status
+        discriminator = member.discriminator
+        display_avatar = member.display_avatar
+        display_icon = member.display_icon
+        display_name = member.display_name
+        dm_channel = member.dm_channel
+        flags = member.flags
+        global_name = member.global_name
+        guild = member.guild
+        guild_avatar = member.guild_avatar
+        guild_permissions = member.guild_permissions
+        id = member.id
+        joined_at = member.joined_at
+        mention = member.mention
+        mobile_status = member.mobile_status
+        mutual_guilds = member.mutual_guilds
+        name = member.name
+        nick = member.nick
+        pending = member.pending
+        premium_since = member.premium_since
+        public_flags = member.public_flags
+        raw_status = member.raw_status
+        resolved_permissions = member.resolved_permissions
+        roles = member.roles
+        status = member.status
+        system = member.system
+        timed_out_until = member.timed_out_until
+        top_role = member.top_role
+        voice = member.voice
+        web_status = member.web_status
+
         userdata = {
-            "member": {
-                "accent_color": member.accent_color,
-                "accent_colour": member.accent_colour,
-                "activities": member.activities,
-                "activity": member.activity,
-                "avatar": member.avatar,
-                "banner": member.banner,
-                "bot": member.bot,
-                "color": member.color,
-                "colour": member.colour,
-                "created_at": str(member.created_at),
-                "default_avatar": member.default_avatar,
-                "desktop_status": member.desktop_status,
-                "discriminator": member.discriminator,
-                "display_avatar": member.display_avatar,
-                "display_icon": member.display_icon,
-                "display_name": member.display_name,
-                "dm_channel": member.dm_channel,
-                "flags": member.flags,
-                "global_name": member.global_name,
-                "guild": member.guild,
-                "guild_avatar": member.guild_avatar,
-                "guild_permissions": member.guild_permissions,
-                "id": member.id,
-                "joined_at": str(member.joined_at),
-                "mention": member.mention,
-                "mobile_status": member.mobile_status,
-                "mutual_guilds": member.mutual_guilds,
-                "name": member.name,
-                "nick": member.nick,
-                "pending": member.pending,
-                "premium_since": str(member.premium_since),
-                "public_flags": member.public_flags,
-                "raw_status": member.raw_status,
-                "resolved_permissions": member.resolved_permissions,
-                "roles": [x.name for x in member.roles if x.name != "@everyone"],
-                "status": member.status,
-                "system": member.system,
-                "timed_out_until": str(member.timed_out_until),
-                "web_status": member.web_status
-            }
+
         }
 
         filedir = f'{os.getcwd}/userdata/{member.id}.txt'
