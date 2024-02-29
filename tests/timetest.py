9326b9bc8515221c956a7a79c3870616552cbf7a
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-02-29 20:23:52 +0100

    mensa fixed

---
+++ tests/timetest.py
@@ -0,0 +1,6 @@
+import datetime
+
+# Eine bestimmte Uhrzeit erstellen (Stunden, Minuten, Sekunden)
+specific_time = datetime.datetime(year=2022, month=2, day=28, hour=14, minute=30, second=0)
+
+print("Spezifische Uhrzeit:", specific_time)
