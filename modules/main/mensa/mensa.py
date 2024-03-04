64255ffa42b9dc8464c1a226232290223ffabbeb
p_heineke <heineke.pe@gmail.com>
2024-03-04 18:19:59 +0100

    major changes - revertpoint

--- modules/main/mensa/mensa.py
+++ modules/main/mensa/mensa.py
@@ -10,66 +10,93 @@ class Mensa(commands.Cog):
         self.user_time_db = database.Manage_database("users.db")
         self.cyclereset.start()
 
-    @commands.command()
-    async def mensatime(self, ctx, equal=None, arg=None):
-        username = ctx.author.name
-        if ctx.prefix == "my.":
+    def db_controller(self, ctxprefix, authormention, authorname, arg=None, equal=None):
+        if ctxprefix == "my.":
             if arg is None and equal is None:
                 try:
-                    response = f"{ctx.author.mention} Deine Mensazeit ist: {self.user_time_db.get_user_times(username)[0]}".replace("'","")
-                    await ctx.send(response)
+                    response = f"{authormention} Deine Mensazeit ist: {self.user_time_db.get_user_times(authorname)[0]}".replace("'","")
+                    return ("1", response) #1, response
                     
                 except:
-                    await ctx.send("Keine Usertime")
+                    return ("3", "Keine Usertime") #3
             else:
                 if equal == "=":
                     if arg in ["none", "NONE", "false", "False"]:
-                        print("hier")
                         try:
-                            self.user_time_db.remove_user(username)
-                            await ctx.message.add_reaction('✅')
+                            self.user_time_db.remove_user(authorname)
+                            return ("2", None) #2
                         except:
-                            await ctx.send("Kein User eingetragen.")
+                            return ("3", "Kein User eingetragen.") #3
                     elif arg in ["const", "constant"]:
                         try:
-                            self.user_time_db.set_user_time_constant(username)
-                            await ctx.message.add_reaction('✅')
+                            self.user_time_db.set_user_time_constant(authorname)
+                            return ("2", None) #2
                         except:
-                            await ctx.send("Kein User eingetragen")
+                            return ("3", "Kein User eingetragen.") #3
                     elif arg in ["nconst", "nconstant", "notconstant"]:
                         try:
-                            self.user_time_db.set_user_time_nconstant(username)
-                            await ctx.message.add_reaction('✅')
+                            self.user_time_db.set_user_time_nconstant(authorname)
+                            return ("2", None) #2
                         except:
-                            await ctx.send("Kein User eingetragen")
+                            return ("3", "Kein User eingetragen.") #3
                     elif arg in ["jetzt", "now", "rn"]:
                         try:
                             now = datetime.now().strftime("%H:%M")
-                            self.user_time_db.save_user_time(username, str(now))
-                            await ctx.message.add_reaction('✅')
+                            self.user_time_db.save_user_time(authorname, str(now))
+                            return ("2", None) #2
                         except:
-                            await ctx.send("Kein gültiger Befehl")
+                            return ("0", "Kein gültiger Befehl") #0
                     else:
                         try:
                             arg = self.user_time_db.striptime(arg)
                             if self.user_time_db.validate_time_format(arg):
-                                self.user_time_db.save_user_time(username, arg)
-                                await ctx.message.add_reaction('✅')
+                                self.user_time_db.save_user_time(authorname, arg)
+                                return ("2", None) #2
                         except Exception as e:
                             print(e)
-                            await ctx.send("Kein gültiger Befehl")
+                            return ("0", "Kein gültiger Befehl") #0
 
                         
-        if ctx.prefix == "xs.":
+        if ctxprefix == "xs.":
             x = self.user_time_db.get_all_users_with_times()
             a = {key: x[key] for key in sorted(x)}
             y = ""
             for key,value in a.items():
-                print(key,value[0])
-                y += f"{value[0]} | {key}\n"
+                y += f"{key:15} |  {value}\n".replace("'","").replace("[","").replace("]","")
+            return ("4", y) #4
 
-            await ctx.send(f"{ctx.author.mention} Folgende Mensazeiten sind eingetragen:\n```\n{y}\n```")
+    @commands.command()
+    async def mensatime(self, ctx, equal=None, arg=None):
+        authorname = ctx.author.name
+        authormention = ctx.author.mention
+        ctxprefix = ctx.prefix
+
+        db_control, response = self.db_controller()
+
+        match db_control, response:
+            case "0", response:
+                await ctx.send(response)
+            case "1", response:
+                await ctx.send(response)
+            case "2", None:
+                await ctx.message.add_reaction('✅')
+            case "3", None:
+                await ctx.send("Keine Usertime")
+            case "4", response:
+                await ctx.send(f"{authormention} Folgende Mensazeiten sind eingetragen:\n```\n{response}\n```")
+        
+
+        
     
+    @commands.Cog.listener("on_reaction_add")
+    async def on_reaction_add(self,reaction, user):
+        if user.bot:
+            return
+        if str(reaction.emoji) == "<:plus1:1171776509195845652>":
+            hookname = self.client.user.name
+            webhook = await self.check_for_webhook(reaction.message, hookname)
+            await webhook.send(content=reaction.message.content, avatar_url=user.avatar, username=user.name)
+
     @tasks.loop(minutes=15.0)
     async def cyclereset(self):
         if datetime.now().strftime("%H") == "15":
