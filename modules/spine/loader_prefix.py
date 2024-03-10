721cb220daed1b8669797b1fea432bbbd786c400
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-03-10 19:22:25 +0100

    .

--- modules/spine/loader_prefix.py
+++ modules/spine/loader_prefix.py
@@ -52,16 +52,6 @@ class Loader_prefix(commands.Cog):
     async def shutdown(self, ctx):
         await ctx.send('Shutting down...')
         print('Shutting down...')
-        slower = True
-        while(slower):
-            try:
-                await self.bot.change_presence(status=discord.Status.idle)
-                await self.bot.user.edit(avatar=open("./lib/pic/offline_pfp.png",'rb').read())
-                slower = False
-                break
-            except:
-                await asyncio.sleep(10.0)
-                slower = True
 
         await self.bot.close()
 
