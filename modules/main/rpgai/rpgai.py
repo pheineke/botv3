0d5f2f16faf5081ce311a207eb657e9b20383bcf
p_heineke <heineke.pe@gmail.com>
2024-03-10 23:53:07 +0100

    .

--- modules/main/rpgai/rpgai.py
+++ modules/main/rpgai/rpgai.py
@@ -235,7 +235,10 @@ Du kannst in der Welt spielen in dem du `.chat` oder `.c` aufrufst und dahinter 
                     try:
                         await self.channel.send(self.game.chat(ingameuser, message))
                     except AttributeError as e:
-                        await self.loadgame(message.channel, )
+                        await self.loadgame(message.channel)
+                    except Exception as e:
+                        print(e)
+
                 else:
                     await self.channel.send("No Ingame Name!")
                 
