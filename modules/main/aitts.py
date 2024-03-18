b1d21c7bf4940c1bf916c634f9618a1b9bf549b3
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-03-18 21:45:50 +0100

    .

--- modules/main/aitts.py
+++ modules/main/aitts.py
@@ -63,23 +63,12 @@ class AiAudio(commands.Cog):
 
     @app_commands.command(name="compose", description="Compose an audio sequence with a prompt")
     @app_commands.describe(length="Länge in Sekunden")
-<<<<<<< HEAD
-    @app_commands.choices(model=[
-        app_commands.Choice(name='musicgen-small', value="facebook/musicgen-small"),
-        app_commands.Choice(name='musicgen-medium', value="facebook/musicgen-medium"),
-        app_commands.Choice(name='musicgen-large', value="facebook/musicgen-large"),
-        app_commands.Choice(name='musicgen-melody', value="facebook/musicgen-melody"),
-        app_commands.Choice(name='audiogen-medium', value="facebook/audiogen-medium")
-    ])
-    async def compose(self, interaction:discord.Interaction, prompt:str, length:int=29, model:app_commands.Choice[str]=None):
-=======
     @app_commands.choices(model_size=[
         app_commands.Choice(name='small', value="facebook/musicgen-small"),
         app_commands.Choice(name='medium', value="facebook/musicgen-medium"),
         app_commands.Choice(name='large', value="facebook/musicgen-large")
     ])
     async def compose(self, interaction:discord.Interaction, prompt:str, length:int=None, model_size:app_commands.Choice[str]=None):
->>>>>>> parent of a945fad (more models)
         if os.path.exists("./audio_gen-out.wav"):
             os.remove("./audio_gen-out.wav")
         
