86b163a94a2889febdd323a0a3d017fb7d5c97b1
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-03-19 23:03:33 +0100

    .

--- modules/main/domina-scp.py
+++ modules/main/domina-scp.py
@@ -141,7 +141,7 @@ class Dominascrp(commands.Cog):
             if (not(days) and not(startday) and not(endday)):
                 tabelle_new = self.filter_data_timespan(data=tabelle, start=None, end=None)
                 print(tabelle_new)
-                print(tabelle_new.keys())
+                print(list(tabelle_new.keys()))
                 lentabelle_new = len(tabelle_new.keys())
                 if lentabelle_new > 8:
                     tabelle_new = dict(list(tabelle_new.keys())[:8])
