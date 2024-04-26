7e23ec0623d5a999b274e8295f58bd7d1a69aa4e
pheineke <heineke.pe@gmail.com>
2024-04-26 15:17:47 +0200

    update llama3 cmd

--- modules/main/llama3_.py
+++ modules/main/llama3_.py
@@ -9,20 +9,21 @@ import torch
 class Llama3(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
-        self.pipeline = transformers.pipeline(
+        
+
+    @app_commands.command(name="llama3", description="Llama3 GPT")
+    async def llama3(self, interaction:discord.Interaction, prompt:str):
+        await interaction.response.send_message("Processing...")
+
+        pipeline = transformers.pipeline(
         "text-generation",
         model="meta-llama/Meta-Llama-3-8B-Instruct",
         model_kwargs={"torch_dtype": torch.bfloat16},
         device="cuda",
         )
 
-    @app_commands.command(name="llama3", description="Llama3 GPT")
-    async def llama3(self, interaction:discord.Interaction, prompt:str):
-        await interaction.response.send_message("Processing...")
-
-        output = self.pipeline(prompt)
+        output = pipeline(prompt)
 
         await interaction.followup.send(output) 
 
