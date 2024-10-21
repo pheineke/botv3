c2fa2399e41093955cb4d601d01bb3d03152dd3c
pheineke <heineke.pe@gmail.com>
2024-10-21 12:53:02 +0200

    timetable stuff

--- modules/main/timetable.py
+++ modules/main/timetable.py
@@ -16,6 +16,35 @@ class Timetable(commands.Cog):
         self.bot = bot
         self.timetable_path = "./lib/data/timetables/"
 
+    @app_commands.command(name="timetables", description="See all Users who have uploaded their timetables")
+    async def timetables(self, interaction: discord.Interaction):
+        users : list = []
+        try:
+            for filename in os.listdir(self.timetable_path):
+                filepath = os.path.join(self.timetable_path, filename)
+                if os.path.isfile(filepath):
+                    print(f'File: {filepath}')
+                    if "timetable_" in filepath:
+                        a = filepath.removeprefix("timetable_")
+                        b = a.removesuffix(".png")
+                        users.append(b)
+                elif os.path.isdir(filepath):
+                    pass
+                    #print(f'Directory: {filepath}')
+
+            message = ""
+
+            # Send the message with the attachment
+            await interaction.response.send_message(
+                message,
+                ephemeral = True  # This makes the message only visible to the user who triggered the command
+            )
+
+        except FileNotFoundError as e:
+            await interaction.response.send_message(f"User timetable doesnt exist", ephemeral=True)
+        except Exception as e:  
+            await interaction.response.send_message(f"Error {e}", ephemeral=True)
+
     @app_commands.command(name="timetable", description="Shows timetable of user if uploaded")
     @app_commands.describe(user_selection="User from this server")
     async def timetable(self, interaction: discord.Interaction, user_selection : discord.Member):
